5122cadfc428a4fd9dbeb700eb9d175bd11494e2
Isaac Mann <isaacplmann@gmail.com>
2017-03-24 16:43:40 -0400

    Updates

--- lib/helpers/getLatest.d.ts
+++
@@ -1,2 +0,0 @@
-import { Observable } from 'rxjs/Observable';
-export declare function getLatest<T>(o$: Observable<T>): T;
--- lib/ngrx-query.d.ts
+++
@@ -1,5 +0,0 @@
-import { NgrxQueryConfig } from './helpers/ngrxQueryConfig';
-import { ModuleWithProviders } from '@angular/core';
-export declare class NgrxQueryModule {
-    static forRoot(config: NgrxQueryConfig): ModuleWithProviders;
-}
